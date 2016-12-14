34df52a0395a6ae228351a1b54ce97aa52224cb6
Richard Hart <richardhart92@gmail.com>
2016-12-14 08:26:43 -0500

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const list = [
+{
+	title: 'React',
+	url: 'https://facebook.github.io/react/',
+	author: 'Jordan Walke',
+	num_comments: 3,
+	points: 4,
+	objectID: 0,
+},
+{
+	title: 'Redux',
+	url: 'https://github.com/reactjs/redux',
+	author: 'Dan Abramov, Andrew Clark',
+	num_comments: 2,
+	points: 5,
+	objectID: 1,
+},
+];
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			list: list,
+			query: '',
+		};
+
+		this.onSearchChange=this.onSearchChange.bind(this)
+	}
+
+	onSearchChange(event) {
+		this.setState({query: event.target.value});
+	}
+	
+	render() {
+		const query = this.state.query;
+		return (
+				<div className="App">
+				<form>
+					<input type="text" value={query} onChange={this.onSearchChange}/>
+				</form>
+				{ this.state.list.map((item) =>
+						<div key={item.objectID}>
+						<span><a href={item.url}>{item.title}</a></span>
+						<span>{item.author}</span>
+						<span>{item.num_comments}</span>
+						<span>{item.points}</span>
+						</div>
+					  )}
+				</div>
+		       );
+	}
+}
+
+export default App;
