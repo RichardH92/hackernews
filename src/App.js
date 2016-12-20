3c95d24b1f00a8c9b3f4dfc69d5b8387a50d9c74
Richard Hart <richardhart92@gmail.com>
2016-12-19 21:02:53 -0500

    Refactored to functional components.

--- src/App.js
+++ src/App.js
@@ -53,35 +53,32 @@ class App extends Component {
 	}
 }
 
-class Search extends Component {
-	render() {
-		const { value, onChange, children } = this.props;
-		return (
-				<form>
-				{children}
-				<input type="text" value={value} onChange={onChange} />
-				</form>
-		       );
-	}
+function Search(props) {
+	const { value, onChange, children } = props;
+	return (
+			<form>
+			{children}
+			<input type="text" value={value} onChange={onChange} />
+			</form>
+	       );
 }
 
-class Table extends Component {
-	render() {
-		const { list, pattern } = this.props;
-		return (
-				<div>
-				{
-					list.filter(isSearched(pattern)).map((item) =>
-							<div key={item.objectID}>
-							<span><a href={item.url}>{item.title}></a></span>
-							<span>{item.author}</span>
-							<span>{item.num_comments}</span>
-							<span>{item.points}</span>
-							</div>
-							)}
-					</div>
-		       );
-	}
+
+function Table(props) {
+	const { list, pattern } = props;
+	return (
+			<div>
+			{
+				list.filter(isSearched(pattern)).map((item) =>
+						<div key={item.objectID}>
+						<span><a href={item.url}>{item.title}></a></span>
+						<span>{item.author}</span>
+						<span>{item.num_comments}</span>
+						<span>{item.points}</span>
+						</div>
+						)}
+				</div>
+	       );
 }
 
 export default App;
