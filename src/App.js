4c8b424bf541f0378d154cdfca01cf6dc334ed54
Richard Hart <richardhart92@gmail.com>
2016-12-19 21:23:06 -0500

    Added css.

--- src/App.js
+++ src/App.js
@@ -43,12 +43,15 @@ class App extends Component {
 	render() {
 		const { query, list } = this.state; 
 		return (
-				<div className="App">
+				<div className="page">
+				<div className="interactions">
 				<Search value={query} onChange={this.onSearchChange} >
 				Search
 				</Search>
+				</div>
 				<Table list={list} pattern={query} />
 				</div>
+				
 		       );
 	}
 }
@@ -61,14 +64,14 @@ const Search = ({ value, onChange, children }) =>
 
 
 const Table = ({ list, pattern }) =>
-<div>
+<div className="table">
 {
 	list.filter(isSearched(pattern)).map((item) =>
-			<div key={item.objectID}>
-			<span><a href={item.url}>{item.title}></a></span>
-			<span>{item.author}</span>
-			<span>{item.num_comments}</span>
-			<span>{item.points}</span>
+			<div key={item.objectID} className="table-row">
+			<span style={{ width: '40%' }}><a href={item.url}>{item.title}></a></span>
+			<span style={{ width: '30%' }}>{item.author}</span>
+			<span style={{ width: '15%' }}>{item.num_comments}</span>
+			<span style={{ width: '15%' }}>{item.points}</span>
 			</div>
 			)
 }
