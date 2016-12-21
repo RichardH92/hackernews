37b1961859da372326f44a2a8072b6a718c83d5f
Richard Hart <richardhart92@gmail.com>
2016-12-20 19:57:14 -0500

    Added test with enzyme.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import App from './App';
 import { Search, Button, Table } from './App';
+import { shallow } from 'enzyme';
 
 describe('App', () => {
 
@@ -66,5 +67,12 @@ describe('Table', () => {
 				);
 		let tree = component.toJSON();
 		expect(tree).toMatchSnapshot();
+	});
+	it('shows two items in list', () => {
+		const element = shallow(
+				<Table { ...props } />
+				);
+
+		expect(element.find('.table-row').length).toBe(2);
 	});
 });
