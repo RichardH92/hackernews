a7d5d2ae6a313a351f935821cb07ab049c582424
Richard Hart <richardhart92@gmail.com>
2016-12-20 20:15:36 -0500

    Added HOC button

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
 			results: null,
 			query: DEFAULT_QUERY,
 			searchKey: '',
+			isLoading: false,
 		};
 
 		this.setSearchTopStories = this.setSearchTopStories.bind(this);
@@ -45,16 +46,19 @@ class App extends Component {
 	setSearchTopStories(result) {
 		const { hits, page } = result;
 		const { searchKey } = this.state;
-		
+
 		const oldHits = page === 0 ? [] : this.state.results[searchKey].hits;
 		const updatedHits = [ ...oldHits, ...hits ];
 
 		this.setState({
-			results : { ...this.state.results, [searchKey]: { hits: updatedHits, page } }
+			results : { ...this.state.results, [searchKey]: { hits: updatedHits, page } },
+			isLoading: false
 		});
 	}
 
 	fetchSearchTopStories(query, page) {
+		this.setState({ isLoading : true });
+
 		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
 			.then(response => response.json())
 			.then(result => this.setSearchTopStories(result));
@@ -71,37 +75,43 @@ class App extends Component {
 	}
 
 	render() {
-		const { query, results, searchKey } = this.state; 
+		const { query, results, searchKey, isLoading } = this.state; 
 		const page = ( results && results[searchKey] && results[searchKey].page) || 0;
 		const list = ( results && results[searchKey] && results[searchKey].hits) || [];
 		return (
-			<div className="page">
-			<div className="interactions">
-			<Search value={query} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
-			Search
-			</Search>
-			</div>
-			<Table list={list} />
-			<div className="interactions">
-				<Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>More</Button>
-			</div>
-			</div>
+				<div className="page">
+				<div className="interactions">
+				<Search value={query} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
+				Search
+				</Search>
+				</div>
+				<Table list={list} />
+				<div className="interactions">
+				<ButtonWithLoading 
+					isLoading={isLoading}
+					onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+					More
+					</ButtonWithLoading>
 
-		);
+				}
+				</div>
+				</div>
+
+		       );
 	}
 }
 
 const Search = ({ value, onChange, onSubmit, children }) =>
-	<form onSubmit={onSubmit}>
-		<input type="text" value={value} onChange={onChange} />
-		<button type="submit">{children}</button>
-		</form>
+<form onSubmit={onSubmit}>
+<input type="text" value={value} onChange={onChange} />
+<button type="submit">{children}</button>
+</form>
 
 
 const Table = ({ list, pattern }) =>
-	<div className="table">
-	{
-		list.map((item) =>	
+<div className="table">
+{
+	list.map((item) =>	
 			<div key={item.objectID} className="table-row">
 			<span style={{ width: '40%' }}><a href={item.url}>{item.title}></a></span>
 			<span style={{ width: '30%' }}>{item.author}</span>
@@ -109,14 +119,21 @@ const Table = ({ list, pattern }) =>
 			<span style={{ width: '15%' }}>{item.points}</span>
 			</div>
 		)
-	}
-	</div>
+}
+</div>
 
 const Button = ({ onClick, children }) =>
-	<button onClick={onClick} type="button">
-	{children}
-	</button>
+<button onClick={onClick} type="button">
+{children}
+</button>
 
+const withLoading = (Component) => ({ isLoading, ...props }) =>
+	isLoading ? <Loading /> : <Component { ...props } />;
+
+const Loading = () =>
+<div>Loading...</div>
+
+const ButtonWithLoading = withLoading(Button);
 
 export default App;
 
